0475a1761c3eab473580cfc37a540a720767f62a
Alina <alinagusarik@gmail.com>
2021-08-18 16:22:24 +0300

    feat : task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    
+    n = Math.abs(n);
+    let a = 0;
+    while (n>0) {
+        a = a* 10+ (n % 10);
+        n = Math.floor(n / 10);
+    }
+     return a;
 }
